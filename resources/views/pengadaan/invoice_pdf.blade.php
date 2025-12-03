fbe2c34f79fc6855539a5fd2d46f1e296f82fd14
obinkmalmsteen <obink.w.malmsteen@gmail.com>
2025-12-03 10:59:07 +0700

    a

--- resources/views/pengadaan/invoice_pdf.blade.php
+++ resources/views/pengadaan/invoice_pdf.blade.php
@@ -38,9 +38,9 @@
         <td style="width:50%; text-align:right; vertical-align:top;">
 
             <div style="display:inline-block; text-align:right;">
-                <img src="{{ asset('mosque/img/logoyayasan.png') }}" style="height: 70px;">
+                <img src="{{ base_path('public/mosque/img/logoyayasan.png') }}" style="height:50px; display:block; margin-left:auto;">
 
-                     style="height:50px; display:block; margin-left:auto;">
+                     
 
                 <div style="font-size:14px; font-weight:700; margin-top:5px;">
                     Cita Amanat Martadiredja
